a863e28a442e39062fad583dbdfefdd1491146c5
atillaertrk <erturk.aa@gmail.com>
2022-09-16 23:28:32 +0300

    first-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  title = 'Ang-Application';
+  name = 'Atilla Ertürk';
 }
